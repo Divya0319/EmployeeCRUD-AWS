d36cafa527490c43f02b0214b05a8bf10d94fc03
Divya Gupta <divygupta0319@gmail.com>
2024-03-12 11:27:09 +0530

    Initial commit of project, with necessary dependencies added, and basic application tested

---
+++ src/main/java/com/example/employeeCrudAws/EmployeeCrudAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeCrudAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeeCrudAws/EmployeeCrudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeCrudAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
