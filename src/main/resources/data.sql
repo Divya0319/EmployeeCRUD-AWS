dc9904dddcf0266d0b112fe6e8580162dda8e218
Divya Gupta <divygupta0319@gmail.com>
2024-03-13 16:40:54 +0530

    Added sql scripts for initializing database and tables beforehand itself

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee(first_name, last_name, age, salary, department) VALUES 
+('Babu', 'Rao', 45, 35000, 'Science'),
+('Hema', 'Rao', 41, 34000, 'Social'),
+('Anna', 'Singh', 35, 25000, 'English'),
+('Jolly', 'Singh', 47, 55000, 'Maths'),
+('Baandiya', 'Yadav', 40, 40000, 'Hindi');
